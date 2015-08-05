4fe3d1044f2dd2f94bb102c463d15f242fecf667
Ivan Mikushin <i.mikushin@gmail.com>
2015-08-05 21:53:05 +0500

    hello world

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+cd $(dirname $0)
+
+export DOCKER_IMAGE=rancher-os-kernel-build
+
+./scripts/ci
+
+rm -rf dist
+
+echo "Build complete. Copying artifacts..."
+DIST_CONTAINER=$(docker create ${DOCKER_IMAGE})
+cleanup() {
+    docker rm -v ${DIST_CONTAINER}
+}
+trap cleanup EXIT
+docker cp ${DIST_CONTAINER}:/source/dist/artifacts dist
+
+ls -l dist/artifacts
